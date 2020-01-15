3b73522813effde7d42b97f241a53c6039ceda61
tray15 <59891954+tray15@users.noreply.github.com>
2020-01-14 17:53:55 -0700

    Exercise 3
    Exercise 3

---
+++ Project1/Project1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string input;
+        double mm;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = string.Format("{0:0.000}", Double.Parse(textBox1.Text) / 25.4);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    class Test
+    {
+        static void Main(String[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
